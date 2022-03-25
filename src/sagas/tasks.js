9c5dcc95280571e50e5a3adbb6d2f15f22194184
ufukucar <ufuk.ucar@tuseb.gov.tr>
2022-03-25 16:24:14 +0300

    =25.03.2022 async Operations or HTTP Requests

--- src/sagas/tasks.js
+++ src/sagas/tasks.js
@@ -1,3 +1,27 @@
+import axios from 'axios'
+import * as actionTypes from '../constants/action-types'
+import { put } from 'redux-saga/effects'
+
+// put = dispatch
+
+// executes when the component dispatches FETCH_TASKS action
 export const fetchTasksWorkerSaga = function* () {
-  console.log('fetch tasks')
+  // http requests
+  yield put({ type: actionTypes.FETCH_TASKS_PENDING })
+
+  try {
+    let response = yield axios.get('http://localhost:7000/tasks')
+
+    console.log('response: ', response)
+    yield put({
+      type: actionTypes.FETCH_TASKS_FULFILLED,
+      payload: response,
+    })
+  } catch (error) {
+    yield put({ type: actionTypes.FETCH_TASKS_REJECTED, payload: error })
+  }
 }
+
+// PENDING
+// FULFILLED
+// REJECTED
