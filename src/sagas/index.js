bc628b41f1f4e9195595f00b05f86a70f29b4a67
ufukucar <ufuk.ucar@tuseb.gov.tr>
2022-03-25 17:50:15 +0300

    =25.03.2022 throttle

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,14 +3,15 @@ import {
   createTaskWorkerSaga,
   deleteTaskWorkerSaga,
 } from './tasks.js'
-import { debounce, takeEvery, takeLatest } from 'redux-saga/effects'
+import { debounce, takeEvery, takeLatest, throttle } from 'redux-saga/effects'
 import * as actionTypes from '../constants/action-types'
 
 export const rootSaga = function* () {
   console.log('rootSaga invoked')
   yield takeLatest(actionTypes.FETCH_TASKS, fetchTasksWorkerSaga)
 
-  yield debounce(500, actionTypes.CREATE_TASK, createTaskWorkerSaga)
+  // yield debounce(1000, actionTypes.CREATE_TASK, createTaskWorkerSaga)
+  yield throttle(1000 * 3, actionTypes.CREATE_TASK, createTaskWorkerSaga)
 
   yield takeEvery(actionTypes.DELETE_TASK, deleteTaskWorkerSaga)
   //FETCH_TASKS --> fetchTasksWorkerSaga
