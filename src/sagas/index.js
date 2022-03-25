8ee4c680b68d1ad8fc61c757534cc2893779e7ad
ufukucar <ufuk.ucar@tuseb.gov.tr>
2022-03-25 17:43:10 +0300

    =25.03.2022 debounce

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,14 +3,14 @@ import {
   createTaskWorkerSaga,
   deleteTaskWorkerSaga,
 } from './tasks.js'
-import { takeEvery, takeLatest } from 'redux-saga/effects'
+import { debounce, takeEvery, takeLatest } from 'redux-saga/effects'
 import * as actionTypes from '../constants/action-types'
 
 export const rootSaga = function* () {
   console.log('rootSaga invoked')
-  yield takeEvery(actionTypes.FETCH_TASKS, fetchTasksWorkerSaga)
+  yield takeLatest(actionTypes.FETCH_TASKS, fetchTasksWorkerSaga)
 
-  yield takeLatest(actionTypes.CREATE_TASK, createTaskWorkerSaga)
+  yield debounce(500, actionTypes.CREATE_TASK, createTaskWorkerSaga)
 
   yield takeEvery(actionTypes.DELETE_TASK, deleteTaskWorkerSaga)
   //FETCH_TASKS --> fetchTasksWorkerSaga
