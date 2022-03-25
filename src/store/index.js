6c2e0d8975b5305268df020606ec746be9919c03
ufukucar <ufuk.ucar@tuseb.gov.tr>
2022-03-25 14:40:21 +0300

    =26.03.2022 redux-saga kuruldu, store icindeki index.js ekleme yapildi

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,25 @@
-import { createStore, applyMiddleware } from "redux";
-import allReducers from "../reducers";
-import { composeWithDevTools } from "redux-devtools-extension";
-import reduxThunk from "redux-thunk";
-import { createLogger } from "redux-logger";
-import reduxPromiseMiddleware from "redux-promise-middleware";
+import { createStore, applyMiddleware } from 'redux'
+import allReducers from '../reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reduxThunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import reduxPromiseMiddleware from 'redux-promise-middleware'
 
-//basic logs
-//var logger = createLogger();
+import createSagaMiddleware from 'redux-saga'
 
-//collapse all logs
-// var logger = createLogger({
-//   collapsed: true
-// });
+//logger
+var logger = createLogger()
 
-//collapse only FETCH_TASKS_REQUEST
-// var logger = createLogger({
-//   collapsed: (getState, action, logEntry) => {
-//     return action.type === actionTypes.FETCH_TASKS_REQUEST
-//   }
-// });
-
-//expand only when state has "error" object
-// var logger = createLogger({
-//   collapsed: (getState, action, logEntry) => {
-//     return !logEntry.nextState.tasks.error;
-//   }
-// });
-
-var logger = createLogger();
+//saga
+let saga = createSagaMiddleware()
 
 //middleware
-let middleware = [ reduxThunk, reduxPromiseMiddleware, logger ];
+let middleware = [reduxThunk, saga, reduxPromiseMiddleware, logger]
 
-var store = createStore(allReducers, composeWithDevTools(applyMiddleware(...middleware)));
-export default store;
+//store
+var store = createStore(
+  allReducers,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware)),
+)
+export default store
